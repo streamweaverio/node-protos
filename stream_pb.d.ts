9f4675e8605c26f22c0d3792922d425f6cd0b578
Tamarakuro Patrick Foh Jr <patrickfoh@gmail.com>
2024-11-11 21:01:57 +0100

    Streamweaver proto v0.1.0: Generated nodejs code from commit: 2f54efc2b645e72ccd67fbc1fc815d19f25bbeaa (#8)
    Auto-merging PR after successful checks

--- stream_pb.d.ts
+++ stream_pb.d.ts
@@ -6,40 +6,9 @@
 
 import * as jspb from "google-protobuf";
 
-export class StreamRetentionOptions extends jspb.Message { 
-    getMaxAge(): string;
-    setMaxAge(value: string): StreamRetentionOptions;
-    getMaxSize(): number;
-    setMaxSize(value: number): StreamRetentionOptions;
-    getRetentionPolicy(): StreamRetentionPolicy;
-    setRetentionPolicy(value: StreamRetentionPolicy): StreamRetentionOptions;
-
-    serializeBinary(): Uint8Array;
-    toObject(includeInstance?: boolean): StreamRetentionOptions.AsObject;
-    static toObject(includeInstance: boolean, msg: StreamRetentionOptions): StreamRetentionOptions.AsObject;
-    static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
-    static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
-    static serializeBinaryToWriter(message: StreamRetentionOptions, writer: jspb.BinaryWriter): void;
-    static deserializeBinary(bytes: Uint8Array): StreamRetentionOptions;
-    static deserializeBinaryFromReader(message: StreamRetentionOptions, reader: jspb.BinaryReader): StreamRetentionOptions;
-}
-
-export namespace StreamRetentionOptions {
-    export type AsObject = {
-        maxAge: string,
-        maxSize: number,
-        retentionPolicy: StreamRetentionPolicy,
-    }
-}
-
 export class Stream extends jspb.Message { 
     getStreamName(): string;
     setStreamName(value: string): Stream;
-
-    hasRetentionOptions(): boolean;
-    clearRetentionOptions(): void;
-    getRetentionOptions(): StreamRetentionOptions | undefined;
-    setRetentionOptions(value?: StreamRetentionOptions): Stream;
 
     serializeBinary(): Uint8Array;
     toObject(includeInstance?: boolean): Stream.AsObject;
@@ -54,18 +23,14 @@ export class Stream extends jspb.Message {
 export namespace Stream {
     export type AsObject = {
         streamName: string,
-        retentionOptions?: StreamRetentionOptions.AsObject,
     }
 }
 
 export class CreateStreamRequest extends jspb.Message { 
     getStreamName(): string;
     setStreamName(value: string): CreateStreamRequest;
-
-    hasRetentionOptions(): boolean;
-    clearRetentionOptions(): void;
-    getRetentionOptions(): StreamRetentionOptions | undefined;
-    setRetentionOptions(value?: StreamRetentionOptions): CreateStreamRequest;
+    getRetentionTimeMs(): number;
+    setRetentionTimeMs(value: number): CreateStreamRequest;
 
     serializeBinary(): Uint8Array;
     toObject(includeInstance?: boolean): CreateStreamRequest.AsObject;
@@ -80,7 +45,7 @@ export class CreateStreamRequest extends jspb.Message {
 export namespace CreateStreamRequest {
     export type AsObject = {
         streamName: string,
-        retentionOptions?: StreamRetentionOptions.AsObject,
+        retentionTimeMs: number,
     }
 }
 
@@ -131,11 +96,6 @@ export class GetStreamResponse extends jspb.Message {
     getName(): string;
     setName(value: string): GetStreamResponse;
 
-    hasRetentionOptions(): boolean;
-    clearRetentionOptions(): void;
-    getRetentionOptions(): StreamRetentionOptions | undefined;
-    setRetentionOptions(value?: StreamRetentionOptions): GetStreamResponse;
-
     serializeBinary(): Uint8Array;
     toObject(includeInstance?: boolean): GetStreamResponse.AsObject;
     static toObject(includeInstance: boolean, msg: GetStreamResponse): GetStreamResponse.AsObject;
@@ -149,11 +109,5 @@ export class GetStreamResponse extends jspb.Message {
 export namespace GetStreamResponse {
     export type AsObject = {
         name: string,
-        retentionOptions?: StreamRetentionOptions.AsObject,
     }
-}
-
-export enum StreamRetentionPolicy {
-    TIME_RETENTION_POLICY = 0,
-    SIZE_RETENTION_POLICY = 1,
 }
